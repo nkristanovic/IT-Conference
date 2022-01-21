63a9a766e4b17c8bc5bdefa20a2ed6fa8afe4f35
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-01-21 14:28:28 +0100

    Added SectionIntro js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import SectionIntro from './components/SectionIntro/SectionIntro';
 
 function App() {
   return (
     <>
+      <SectionIntro />
     </>
   );
 }
--- src/lib/style/theme.js
+++ src/lib/style/theme.js
@@ -1,9 +1,10 @@
 export const breakpoints = {
     mobile: 'max-width: 575px',
-    desktop: 'min-width: 1024px',
+    desktop: 'min-width: 1440px',
 };
 
 export const colors = {
     white: '#ffffff',
     black: '#000000',
+    gray: '#333333'
 };
