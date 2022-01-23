7b387678ab231ccf3481b699e9170a155909d59a
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-01-23 01:32:05 +0100

    Added animation and mock js to section intro

---
+++ src/lib/mock/section.js
@@ -0,0 +1,34 @@
+export const introSectionMock = [
+    {
+        id: 1,
+        title: 'About the conference',
+        description: 'Shape is a 3 day ICT conference bringing you top of the line keynote speakers in both design and development. Additionally, we are happy to have great workshops, many networking activities and a party to remember!'
+    }
+];
+
+export const introMock = [
+    {
+        id: 1,
+        itemTitle: 'PD',
+        itemSubtitle: 'Product Design',
+        itemText: 'Find out the latest trends and buzz for experience research'
+    },
+    {
+        id: 2,
+        itemTitle: 'UX',
+        itemSubtitle: 'User experience',
+        itemText: 'Find out the latest trends and buzz for experience research'
+    },
+    {
+        id: 3,
+        itemTitle: 'PD',
+        itemSubtitle: 'Frontend development',
+        itemText: 'Find out the latest trends and buzz for experience research'
+    },
+    {
+        id: 4,
+        itemTitle: 'MD',
+        itemSubtitle: 'Mobile development',
+        itemText: 'Find out the latest trends and buzz for experience research'
+    }
+];
